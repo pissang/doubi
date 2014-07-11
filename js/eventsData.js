844f190018d2b0dbfde874ae30df4d23a0a7b641
lang <bm2736892@gmail.com>
2014-07-11 11:37:27 +0800

    events data

--- js/eventsData.js
+++ js/eventsData.js
@@ -1,2 +1,66 @@
-﻿define({});
-
+﻿define({
+    "2010世界杯": [
+        {
+            image: 'pictures/法国队2010内讧1.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧2.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧3.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧4.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧5.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧6.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧7.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧8.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/法国队2010内讧9.jpg',
+            description: '',
+        },
+    ],
+    "太太团": [ 
+        {
+            image: 'pictures/太太团1.jpg',
+            description: '萨尼亚太太热情与粉丝合影',
+        },
+        {
+            image: 'pictures/太太团2.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/太太团3.jpg',
+            description: '法国小组赛最后一轮对阵厄瓜多尔，场边法国太太团群芳争艳，甚至完全抢了比赛的风头',
+        },
+        {
+            image: 'pictures/太太团4.jpg',
+            description: '',
+        },
+        {
+            image: 'pictures/太太团5.jpg',
+            description: '八分之一决赛德国与阿尔及利亚之战进行，德国太太团现场助威',
+        },
+        {
+            image: 'pictures/太太团6.jpg',
+            description: '意大利太太团场边拍照',
+        }
+    ]
+});
